9b08df8e46916457ce9aaa7c7bedfb9275806dcb
Isaac Carvalho <carvalhoaisaac@gmail.com>
2024-01-26 19:56:36 -0300

    servidor http e api finalizados

---
+++ src/server/server.ts
@@ -0,0 +1,31 @@
+import Koa, { Context } from 'koa';
+import Router from 'koa-router';
+import koaBody from 'koa-body';
+
+const server = new Koa();
+const router = new Router();
+
+router.get('/users', (ctx: Context, next: Function) => {
+    const users = [
+        {
+            _id: 0,
+            name: 'Isaac Carvalho'
+        }
+    ];
+
+    ctx.body = users;
+});
+
+router.post('/users', (ctx: Context, next: Function) => {
+    const payloadUser = ctx.request.body;
+    const newUser = { _id: 1, ...payloadUser }
+    
+    ctx.body = newUser;
+})
+
+server.use(koaBody());
+server.use(router.routes());
+
+server.listen(3000, () => {
+    console.log('🔥 HTTP Server runing!')
+})
