cdf76944bef97e2ff12e0057276e9406f10e0c4a
Isaac Carvalho <carvalhoaisaac@gmail.com>
2024-01-28 22:03:00 -0300

    auth middleware finished

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,35 @@
 import Koa, { Context } from 'koa';
 import Router from 'koa-router';
 import koaBody from 'koa-body';
+import jwt from 'jsonwebtoken';
 
 const server = new Koa();
 const router = new Router();
 
-router.get('/users', (ctx: Context, next: Function) => {
+const generateToken = () => {
+    const secret: any = process.env.SECRET;
+
+    const token = jwt.sign({ id: 0}, secret);
+    return token;
+}
+
+const autenticateMiddleware = (ctx: Context, next: Function) => {
+    const token  = ctx.header.authorization || '';
+    const secret: any = process.env.SECRET;
+
+    console.log('aqui')
+    try {
+        jwt.verify(token, secret);
+    } catch (err) {
+        ctx.body = { error: 'invalid token' }
+        ctx.status = 401
+    }
+   
+}
+
+router.all(/^\/api\/v1\//gim, autenticateMiddleware);
+
+router.get('/api/v1/users', (ctx: Context, next: Function) => {
     const users = [
         {
             _id: 0,
@@ -16,7 +40,7 @@ router.get('/users', (ctx: Context, next: Function) => {
     ctx.body = users;
 });
 
-router.post('/users', (ctx: Context, next: Function) => {
+router.post('/api/v1/users', (ctx: Context, next: Function) => {
     const payloadUser = ctx.request.body;
     const newUser = { _id: 1, ...payloadUser }
     
@@ -24,8 +48,9 @@ router.post('/users', (ctx: Context, next: Function) => {
 });
 
 server.use(koaBody());
-server.use(router.routes());
+server.use(router.routes())
+.use(router.allowedMethods());;
 
 server.listen(3000, () => {
     console.log('🔥 HTTP Server runing!')
-})
+});
